791dfce84b19a5468063a0f669315f85fe5cc1ca
irislamov <islamov.ildar@gmail.com>
2020-03-13 18:05:29 +0500

    Практическое задание по первому уроку JS-2 выполнено.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const products = [
+    {id: 1, title: "Notebook", price: 20000},
+    {id: 2, title: "Mouse", price: 1500},
+    {id: 3, title: "Keyboard", price: 5000},
+    {id: 4, title: "Gamepad", price: 4500},
+];
+
+const renderProduct = (title, price, img = "images/ico.png") => {
+    return `<div class="product-item">
+            <div class="product-description">
+            <h3>${title}</h3>
+            <p><h3>${price}</h3></p>
+            <button class="buy-btn">Добавить в корзину</button>
+            </div>
+            <div class="product-img"><img src= ${img} alt="product-photo"></div>    
+            </div>
+            `;
+};
+
+const renderProducts = list => {
+    const productList = list.map(item => renderProduct(item.title, item.price));
+    document.querySelector('.products').innerHTML = productList.join("");
+};
+
+renderProducts(products);
